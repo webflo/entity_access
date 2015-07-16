e9640003e44671d74ab416dd2426ca974323fe0c
Florian Weber <florian.weber@ueberbit.de>
2015-07-16 19:40:15 +0200

    Add generic EACH with grants.

--- src/GrantBasedEntityAccessControlHandler.php
+++ src/GrantBasedEntityAccessControlHandler.php
@@ -8,9 +8,36 @@
 namespace Drupal\entity_access;
 
 use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Session\AccountInterface;
 
 class GrantBasedEntityAccessControlHandler extends EntityAccessControlHandler implements GrantBasedEntityAccessControlHandlerInterface {
 
   use GrantBasedEntityAccessTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, $langcode, AccountInterface $account) {
+    $result = parent::checkAccess($entity, $operation, $langcode, $account);
+
+    if ($result->isNeutral()) {
+      // Evaluate entity grants.
+
+      /**
+       * @todo: Not sure about this logic,
+       */
+      if ($langcode == LanguageInterface::LANGCODE_DEFAULT) {
+        $langcode = $entity->language()->getId();
+      }
+
+      $result = $this->grantStorage()->access($entity, $operation, $langcode, $account);
+      return $result;
+    }
+
+    return $result;
+  }
+
 
 }

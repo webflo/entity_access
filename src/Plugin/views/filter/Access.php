d34b45afdd6464b0c35b2eef797577fe964e578f
Florian Weber <florian.weber@ueberbit.de>
2017-11-22 11:54:36 +0100

    Add a views filter for entity access

---
+++ src/Plugin/views/filter/Access.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\entity_access\Plugin\views\filter\Access.
+ */
+
+namespace Drupal\entity_access\Plugin\views\filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * Filter by entity access records.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("entity_grant_access")
+ */
+class Access extends FilterPluginBase {
+
+  public function adminSummary() {
+  }
+
+  protected function operatorForm(&$form, FormStateInterface $form_state) {
+  }
+
+  public function canExpose() {
+    return FALSE;
+  }
+
+  /**
+   * See _entity_access_access_where_sql() for a non-views query based implementation.
+   */
+  public function query() {
+    $account = $this->view->getUser();
+    if (!$account->hasPermission('bypass entity access')) {
+      $table = $this->ensureMyTable();
+      $grants = db_or();
+      foreach (entity_access_grants('view', $account) as $realm => $gids) {
+        foreach ($gids as $gid) {
+          $grants->condition(db_and()
+            ->condition($table . '.gid', $gid)
+            ->condition($table . '.realm', $realm)
+          );
+        }
+      }
+
+      $this->query->addWhere('AND', $grants);
+      $this->query->addWhere('AND', $table . '.grant_view', 1, '>=');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    $contexts = parent::getCacheContexts();
+
+    // entity_access access is potentially cacheable per user.
+    $contexts[] = 'user';
+
+    return $contexts;
+  }
+
+}
